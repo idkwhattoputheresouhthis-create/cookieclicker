dcc0fb66e410ed1c995e6e33dddc0190a935a9d0
idkwhattoputheresouhthis-create <idkwhattoputheresouhthis@gmail.com>
2026-09-15 11:19:58 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2080,14 +2080,7 @@ Game.Launch=function()
 		else Game.Loader.loaded=callback;
 		Game.Loader.Load(['filler.png']);
 	}
-	Game.ErrorFrame=function()
-	{
-		l('offGameMessage').innerHTML=
-		'<div class="title">Oops. Wrong address!</div>'+
-		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
-		'<small>(If for any reason, you are unable to access the game on the official URL, you can also try this <a href="//cookieclicker.eu/cookieclicker/" target="_blank">secondary URL</a>.)</small></div>';
-	}
+	 // Do nothing
 	Game.timedout=false;
 	Game.Timeout=function()
 	{
